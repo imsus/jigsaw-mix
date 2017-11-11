70dc63f4ba40ed6853110d94687ec3efdc60d157
Imam Susanto <abc.imams@gmail.com>
2017-11-11 11:19:31 +0700

    Initial Commit

---
+++ source/index.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.master')
+
+@section('body')
+<div class="min-h-screen bg-yellow flex items-center justify-center">
+  <div class="border py-6 px-8 border-black border-2 lg:w-1/3 md:w-1/2 sm:w-full bg-white mx-4">
+    <h1 class="font-sans mb-3">Jigsaw Mix</h1>
+    <p class="mb-4 leading-normal">Jigsaw Static Site Generator boilerplate with additional Laravel Mix and Tailwind CSS flavor</p>
+    <div>
+      <a class="no-underline inline-block py-2 px-3 border border-black border-2 mr-3 bg-black text-white uppercase font-bold text-sm tracking-wide" href="">Github</a>
+      <a class="no-underline inline-block py-2 px-3 border border-black border-2 bg-grey-dark text-white uppercase font-bold text-sm tracking-wide" href="">Jigsaw Docs</a>
+    </div>
+  </div>
+</div>
+@endsection
---
+++ webpack.mix.js
@@ -0,0 +1,79 @@
+let mix = require('laravel-mix')
+var tailwind = require('tailwindcss')
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix
+  .js('source/_assets/js/index.js', 'source/js')
+  .postCss('source/_assets/css/style.css', 'source/css', [
+    tailwind('tailwind.js')
+  ])
+  .options({
+    purifyCss: true
+  })
+  .browserSync({
+    proxy: false,
+    server: 'build_local',
+    files: [
+      'build_local/**/*',
+      {
+        match: ['source/_layouts/**/*', 'source/_assets/**/*', 'source/**/*.blade.php'],
+        fn: function (event, file) {
+          if (event === 'change') {
+            const exec = require('child_process').exec
+            exec('jigsaw build', (error, stdout, stderr) => {
+              if (error) {
+                console.error(`exec error: ${error}`)
+                return
+              }
+              console.log(`${stdout}`)
+              console.log(`${stderr}`)
+            })
+          }
+        }
+      }
+    ]
+  })
+
+// Full API
+// mix.js(src, output);
+// mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
+// mix.ts(src, output); <-- Requires tsconfig.json to exist in the same folder as webpack.mix.js
+// mix.extract(vendorLibs);
+// mix.sass(src, output);
+// mix.standaloneSass('src', output); <-- Faster, but isolated from Webpack.
+// mix.fastSass('src', output); <-- Alias for mix.standaloneSass().
+// mix.less(src, output);
+// mix.stylus(src, output);
+// mix.postCss(src, output, [require('postcss-some-plugin')()]);
+// mix.browserSync('my-site.dev');
+// mix.combine(files, destination);
+// mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
+// mix.copy(from, to);
+// mix.copyDirectory(fromDir, toDir);
+// mix.minify(file);
+// mix.sourceMaps(); // Enable sourcemaps
+// mix.version(); // Enable versioning.
+// mix.disableNotifications();
+// mix.setPublicPath('path/to/public');
+// mix.setResourceRoot('prefix/for/resource/locators');
+// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
+// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
+// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
+// mix.options({
+//   extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
+//   globalVueStyles: file, // Variables file to be imported in every component.
+//   processCssUrls: true, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
+//   purifyCss: false, // Remove unused CSS selectors.
+//   uglify: {}, // Uglify-specific options. https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+//   postCss: [] // Post-CSS options: https://github.com/postcss/postcss/blob/master/docs/plugins.md
+// });
